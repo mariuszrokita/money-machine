97c6c6a94ea1d8aa4f66f7fd0521f20576cef3a6
Mariusz Rokita <mariusz.rokita@gmail.com>
2016-09-28 07:46:18 +0200

    Renamed folder names.

---
+++ Udacity/lesson_01_04/statistical_analysis.py
@@ -0,0 +1,14 @@
+"""Some comment on the module."""
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import Udacity.lesson_01_02.build_dataframes as l0102
+
+
+def test_run():
+    pass
+
+if __name__ == "__main__":
+    test_run()
+
